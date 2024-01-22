37bebd41b9542099bfe06c8427d2b71007901972
Siming Feng <siming@Siming-MBP.local>
2024-01-22 14:32:51 -0500

    added more tests

--- test_delegated_sharing.py
+++ test_delegated_sharing.py
@@ -52,8 +52,31 @@ class DelegatedSharingTests(unittest.TestCase):
         self.assertEqual(u2_file, b'shared data')
         self.assertEqual(u3_file, b'shared data')
     
+    def test_chain_receive(self):
+        """
+        Test sharing chain 1 -> 2 -> 3
+        Check that 2 needs to receive file before sharing with 3
+        """
+
+        create_user("usr1", "pswd")
+        create_user("usr2", "pswd")
+        create_user("usr3", "pswd")
+        
+        u1 = authenticate_user("usr1", "pswd")
+        u2 = authenticate_user("usr2", "pswd")
+        u3 = authenticate_user("usr3", "pswd")
+
+        u1.upload_file("shared_file", b'shared data')
+        u1.share_file("shared_file", "usr2")
+        
+        with self.assertRaises(util.DropboxError):
+            u2.share_file("shared_file", "usr3")
+    
     def test_chain_revoke(self):
-        """Test sharing chain 1 -> 2 -> 3, both 2 & 3 lose access after 1 revokes file from 2"""
+        """
+        Test sharing chain 1 -> 2 -> 3
+        Both 2 & 3 lose access after 1 revokes file from 2
+        """
         
         create_user("usr1", "pswd")
         create_user("usr2", "pswd")
@@ -71,39 +94,123 @@ class DelegatedSharingTests(unittest.TestCase):
         
         u1.revoke_file("shared_file", "usr2")
         
-        try:
+        with self.assertRaises(util.DropboxError):
             u2.download_file("shared_file")
-            self.fail("Test Failed: User 2 should not be able to download file")
-        except util.DropboxError:
-            pass
         
-        try:
+        with self.assertRaises(util.DropboxError):
             u3.download_file("shared_file")
-            self.fail("Test Failed: User 3 should not be able to download file")
-        except util.DropboxError:
-            pass
         
-    def test_tree(self):
-        """Test sharing tree 1 => 2, 3; 2 => 4, 5; 3 => 6, 7"""
+    def test_tree_DFS(self):
+        """
+        Test sharing files DFS style
+        1 -> 2 -> 4
+               -> 5
+          -> 3 -> 6
+               -> 7
+        """
+        users = []
+        for i in range(7):
+            create_user(f"usr{i+1}", "pswd")
+            u = authenticate_user(f"usr{i+1}", "pswd")
+            users.append(u)
         
-        create_user("usr1", "pswd")
-        create_user("usr2", "pswd")
-        create_user("usr3", "pswd")
-        create_user("usr4", "pswd")
-        create_user("usr5", "pswd")
-        create_user("usr6", "pswd")
-        create_user("usr7", "pswd")
-        
-        u1 = authenticate_user("usr1", "pswd")
-        u2 = authenticate_user("usr2", "pswd")
-        u3 = authenticate_user("usr3", "pswd")
-        u4 = authenticate_user("usr4", "pswd")
-        u5 = authenticate_user("usr5", "pswd")
-        u6 = authenticate_user("usr6", "pswd")
-        u7 = authenticate_user("usr7", "pswd")
+        u1, u2, u3, u4, u5, u6, u7 = users
         
         u1.upload_file("shared_file", b'shared data')
         
+        u1.share_file("shared_file", "usr2")
+        u2.receive_file("shared_file", "usr1")
+        u2.share_file("shared_file", "usr4")
+        u4.receive_file("shared_file", "usr2")
+        u2.share_file("shared_file", "usr5")
+        u5.receive_file("shared_file", "usr2")
+        u1.share_file("shared_file", "usr3")
+        u3.receive_file("shared_file", "usr1")
+        u3.share_file("shared_file", "usr6")
+        u6.receive_file("shared_file", "usr3")
+        u3.share_file("shared_file", "usr7")
+        u7.receive_file("shared_file", "usr3")
+        
+        for user in users:
+            file = user.download_file("shared_file")
+            self.assertEqual(file, b'shared data')
+    
+    def test_tree_BFS(self):
+        """
+        Test sharing files BFS style
+        1 -> 2 -> 4
+               -> 5
+          -> 3 -> 6
+               -> 7
+        """
+        users = []
+        for i in range(7):
+            create_user(f"usr{i+1}", "pswd")
+            u = authenticate_user(f"usr{i+1}", "pswd")
+            users.append(u)
+        
+        u1, u2, u3, u4, u5, u6, u7 = users
+        
+        u1.upload_file("shared_file", b'shared data')
+        
+        u1.share_file("shared_file", "usr2")
+        u1.share_file("shared_file", "usr3")
+        u2.receive_file("shared_file", "usr1")
+        u3.receive_file("shared_file", "usr1")
+        u2.share_file("shared_file", "usr4")
+        u2.share_file("shared_file", "usr5")
+        u3.share_file("shared_file", "usr6")
+        u3.share_file("shared_file", "usr7")
+        u4.receive_file("shared_file", "usr2")
+        u5.receive_file("shared_file", "usr2")
+        u6.receive_file("shared_file", "usr3")
+        u7.receive_file("shared_file", "usr3")
+        
+        for user in users:
+            file = user.download_file("shared_file")
+            self.assertEqual(file, b'shared data')
+    
+    def test_tree_revoke(self):
+        users = []
+        for i in range(7):
+            create_user(f"usr{i+1}", "pswd")
+            u = authenticate_user(f"usr{i+1}", "pswd")
+            users.append(u)
+        
+        u1, u2, u3, u4, u5, u6, u7 = users
+        
+        u1.upload_file("shared_file", b'shared data')
+        
+        u1.share_file("shared_file", "usr2")
+        u1.share_file("shared_file", "usr3")
+        u2.receive_file("shared_file", "usr1")
+        u3.receive_file("shared_file", "usr1")
+        u2.share_file("shared_file", "usr4")
+        u2.share_file("shared_file", "usr5")
+        u3.share_file("shared_file", "usr6")
+        u3.share_file("shared_file", "usr7")
+        u4.receive_file("shared_file", "usr2")
+        u5.receive_file("shared_file", "usr2")
+        u6.receive_file("shared_file", "usr3")
+        u7.receive_file("shared_file", "usr3")
+        
+        u1.revoke_file("shared_file", "usr2")
+        
+        with self.assertRaises(util.DropboxError):
+            u2.download_file("shared_file")
+            
+        with self.assertRaises(util.DropboxError):
+            u4.download_file("shared_file")
+            
+        with self.assertRaises(util.DropboxError):
+            u5.download_file("shared_file")
+        
+        file = u3.download_file("shared_file")
+        self.assertEqual(file, b'shared data')
+        file = u6.download_file("shared_file")
+        self.assertEqual(file, b'shared data')
+        file = u7.download_file("shared_file")
+        self.assertEqual(file, b'shared data')
 
 
 # Start the REPL if this file is launched as the main program
